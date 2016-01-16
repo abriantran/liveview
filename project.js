3f9c7ffc0cb2c1ae40cccb6bbd6b2aaffc6900c9
Brian Tran <brian@Brians-MacBook-Pro.local>
2016-01-16 16:12:11 -0600

    Make API call for one YouTube comment.

--- project.js
+++ project.js
@@ -1,10 +1,22 @@
 console.log('project.js loaded');
+var videoId = 'sGbxmsDFVnE'
+
+function getComments() {
+    gapi.client.youtube.commentThreads.list({'part': 'snippet', 'videoId': videoId, 'maxResults': 1}).then(function(resp) {
+        console.log(resp.result);
+    }, function(reason) {
+        console.log('Error: ' + reason.result.error.message);
+});
+}
 
 function OnLoadCallback() {
-	console.log('Init function called.');
-	var secret = JSON.parse(secrets);
+    console.log('Init function called.');
+    var secret = JSON.parse(secrets);
 
-	gapi.client.setApiKey(secret.api_key); 
-	gapi.client.load('youtube', 'v3').then(function() { console.log('API loaded.'); });
+    gapi.client.setApiKey(secret.api_key); 
+    gapi.client.load('youtube', 'v3').then(function() {
+         console.log('API loaded.'); 
+         getComments();
+    });
 
 }
